7283779f9db255fc7ceba4fb019a1287169ebfb4
TOXIIIICO <156939831+TOXIIIICO@users.noreply.github.com>
2024-02-27 22:22:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TOXIIIICO/I0I",
+    "https://github.com/TOXIIIICO/God",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
